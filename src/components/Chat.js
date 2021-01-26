79243c01bf948d18c27f494f3a4b029dc2787ff4
brandonpessman <pessman.brandon@gmail.com>
2021-01-26 00:52:39 -0600

    Changes

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -9,6 +9,7 @@ const socket = io('http://localhost:8002/1', {transports: ['websocket']});
 function Chat() {
     const [user, setUser] = useState('')
     const [isLoading, setLoading] = useState(false)
+    const [messages, setMessages] = useState([])
     const [cookies, setCookie] = useCookies(['token']);
     let history = useHistory();
 
@@ -21,6 +22,15 @@ function Chat() {
             socket.on('recieve message', function (chat) {
               console.log("Message from server.")
               console.log(chat)
+
+              const from = chat.from
+              const message = chat.message
+              const time = chat.time
+
+              let list = messages
+              list.push({from, message, time})
+              setMessages(list)
+              console.log(messages)
             })
 
             // Get the input field
@@ -60,13 +70,17 @@ function Chat() {
         <>
             <input type="text" id="chatbox" placeholder="Send a message..." />
             <button className="primary-button" onClick={handleClick} style={{  position: 'absolute', margin: '20px', right: 0, top: 0}}>Logout</button>
-            <div className="message">
+            {/* <div className="message">
                 <h2 style={{color: 'red', margin: '0 14px', fontSize: '16px', fontWeight: '500'}}>{user.username} has connected to chat.<span style={{fontSize: '12px', color: 'grey', marginLeft: '10px', fontWeight: '400'}}>Today at 9:18 AM</span></h2>
-            </div>
-            <div className="message">
-                <h2 style={{color: '#FDD171', margin: '0 14px', fontSize: '16px', fontWeight: '500'}}>Brandon<span style={{fontSize: '12px', color: 'grey', marginLeft: '10px', fontWeight: '400'}}>Today at 9:18 AM</span></h2>
-                <p style={{color: '#DCDDDE', margin: '0 14px', fontSize: '16px', fontWeight: '400'}}>Hey man! How are you doing?</p>
-            </div>
+            </div> */}
+            {messages.map((item) => {
+                return (
+                    <div className="message">
+                        <h2 style={{color: '#FDD171', margin: '0 14px', fontSize: '16px', fontWeight: '500'}}>{item.from}<span style={{fontSize: '12px', color: 'grey', marginLeft: '10px', fontWeight: '400'}}>{item.time}</span></h2>
+                        <p style={{color: '#DCDDDE', margin: '0 14px', fontSize: '16px', fontWeight: '400'}}>{item.message}</p>
+                    </div>
+                )
+            })}
         </>
         }
     </div>
